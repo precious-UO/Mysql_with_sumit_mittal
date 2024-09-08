f737cd6be89b0d761c1c7f8173fcfdc714dac2ae
precious_UO <4preciousokekeani@gmail.com>
2024-09-08 01:39:00 -0700

    Lessons on Mysql

--- Mysql_Queries.sql
+++ Mysql_Queries.sql
@@ -19,15 +19,23 @@
 -- CREATE DATABASE IF NOT EXISTS trendytech;
 USE trendytech;
 
--- =================================== CREATING TABLES AND INSERTING VALUES INTO THE TABLE ===============================================
--- CREATE TABLE IF NOT EXISTS employee (
-	-- First_name VARCHAR(50),
-    -- Middle_name VARCHAR(50),
-    -- Last_name VARCHAR(50),
-    -- Age INT,
-    -- Salary INT,
-    -- Location VARCHAR(50)
--- );
+-- =================================== CREATING A TABLE AND INSERTING VALUES INTO THE TABLE ===============================================
+CREATE TABLE IF NOT EXISTS employee (
+	First_name VARCHAR(50) NOT NULL,
+    Middle_name VARCHAR(50),
+    Last_name VARCHAR(50) NOT NULL,
+    Age INT NOT NULL,
+    Salary INT NOT NULL,
+    Location VARCHAR(50) NOT NULL
+);
+-- DESC employee;
+
+-- INSERT INTO employee (First_name, Middle_name, Last_name, Age, Salary, Location) 
+-- VALUES ('Kapil', 'Kumar', 'Sharma', 28, 10000, 'Bangalore');
+-- INSERT INTO employee (First_name, Middle_name, Last_name, Age, Salary, Location) 
+-- VALUES ('Satish', 'Kumar', 'Sharma', 28, 10000, 'Bangalore');
+-- INSERT INTO employee (First_name, Last_name, Age, Salary, Location) 
+-- VALUES ('Rajesh', 'Sharma', 28, 20000, 'Bangalore');
 
 
 SELECT *
