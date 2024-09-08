a9ed5d7386745a24ebe6765a66170c7abb80ac5f
precious_UO <4preciousokekeani@gmail.com>
2024-09-08 08:13:56 -0700

    Lessons on Mysql

--- Mysql_Queries.sql
+++ Mysql_Queries.sql
@@ -20,24 +20,43 @@
 USE trendytech;
 
 -- =================================== CREATING A TABLE AND INSERTING VALUES INTO THE TABLE ===============================================
-CREATE TABLE IF NOT EXISTS employee (
-	First_name VARCHAR(50) NOT NULL,
-    Middle_name VARCHAR(50),
-    Last_name VARCHAR(50) NOT NULL,
-    Age INT NOT NULL,
-    Salary INT NOT NULL,
-    Location VARCHAR(50) NOT NULL
-);
--- DESC employee;
+-- CREATE TABLE IF NOT EXISTS employee (
+	-- emp_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	-- First_name VARCHAR(50) NOT NULL,
+    -- Middle_name VARCHAR(50),
+    -- Last_name VARCHAR(50) NOT NULL,
+    -- Age INT NOT NULL,
+    -- Salary INT NOT NULL,
+    -- Location VARCHAR(50) NOT NULL DEFAULT 'Bangalore'
+-- );
 
--- INSERT INTO employee (First_name, Middle_name, Last_name, Age, Salary, Location) 
--- VALUES ('Kapil', 'Kumar', 'Sharma', 28, 10000, 'Bangalore');
--- INSERT INTO employee (First_name, Middle_name, Last_name, Age, Salary, Location) 
--- VALUES ('Satish', 'Kumar', 'Sharma', 28, 10000, 'Bangalore');
+-- INSERT INTO employee (First_name, Last_name, Age, Salary) 
+-- VALUES ('Kapil', 'Sharma', 28, 10000);
+-- INSERT INTO employee (First_name, Last_name, Age, Salary, Location) 
+-- VALUES ('Satish', 'Sharma', 28, 10000, 'Hyderabad');
 -- INSERT INTO employee (First_name, Last_name, Age, Salary, Location) 
 -- VALUES ('Rajesh', 'Sharma', 28, 20000, 'Bangalore');
+-- INSERT INTO employee (First_name, Middle_name, Last_name, Age, Salary, Location) 
+-- VALUES ('Rajesh', 'Human', 'Sharma', 28, 20000, 'Bangalore');
+
+-- =============================================== FILTERING DATA IN A TABLE ========================================================================================================
+-- SELECT *
+-- FROM employee;
+
+-- SELECT First_name, Last_name
+-- FROM employee;
+
+-- SELECT *
+-- FROM employee
+-- WHERE age > 29;
+
+-- SELECT *
+-- FROM employee
+-- WHERE BINARY First_name = 'Mannesh';
+
+-- SELECT Location AS Country
+-- FROM employee
+-- WHERE Age <= 28;
 
 
-SELECT *
-FROM employee;
-
+-- ============================================== UPDATING VALUES IN A TABLE =====================================================
