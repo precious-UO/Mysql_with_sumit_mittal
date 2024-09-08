e0ca2482f98cf0423aa205b5e13a64ef8ad0f1de
precious_UO <4preciousokekeani@gmail.com>
2024-09-08 00:41:17 -0700

    Lessons on Mysql

--- Mysql_Queries.sql
+++ Mysql_Queries.sql
@@ -1,1 +1,23 @@
--- MYSQL BY SUMIT MITTAL ----------------------------------------------------
+-- ------------------------------------------------- MYSQL BY SUMIT MITTAL --------------------------------------------------------------------------------------------------
+
+-- SHOW DATABASES;
+-- CREATE DATABASE IF NOT EXISTS trendyTech;
+-- SHOW DATABASES;
+-- DROP DATABASE IF EXISTS trendyTech;
+-- CREATE DATABASE IF NOT EXISTS trendyTech;
+-- USE trendyTech;
+-- CREATE TABLE IF NOT EXISTS employee (
+	-- Name VARCHAR(50),
+    -- Age INT,
+    -- Salary INT
+-- );
+-- SHOW TABLES;
+-- DESC employee;
+-- DROP TABLE IF EXISTS employee;
+
+-- ================================================= CREATING A DATABASE AND USING IT ========================================
+-- CREATE DATABASE IF NOT EXISTS trendytech;
+USE trendytech;
+
+-- =================================== CREATING TABLES AND INSERTING VALUES INTO THE TABLE ===============================================
+
